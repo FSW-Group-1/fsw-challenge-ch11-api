5ad5afb4652d2f8021f96df52528f2af0fa4506c
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-03-29 18:03:19 +0700

    Update database config

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variable": "DATABASE_URL",
+    "use_env_variable": "HEROKU_POSTGRESQL_ORANGE_URL",
     "dialect": "postgres",
     "operatorsAliases": false,
     "protocol": 'postgres',
