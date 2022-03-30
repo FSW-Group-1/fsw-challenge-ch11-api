ef29a94c444e4f728af83256e66e2d4889b2eea4
FAUZAN HIBATUR RAHMAN <2106221@dbc.co.id>
2022-03-30 14:31:55 +0700

    all tdd task fauzan

--- test/authTest.spec.js
+++ test/authTest.spec.js
@@ -27,7 +27,6 @@ describe('POST /api/login', () => {
     test('attempt login success', done => {
         const email = 'jk@gmail.com';
         const password = '123456';
-        // const username = 'dnd'
         request(app)
             .post('/api/login')
             .send({ email, password})
@@ -63,6 +62,80 @@ describe('GET /api/verifyToken', () => {
                     expect(res.body.data).toHaveProperty('email')
                     expect(res.body.data.id).toEqual(id)
                     expect(res.body.data.email).toEqual(email)
+                    done();
+                })
+        })
+    })
+
+    describe('GET /api/me', () => {
+        test('test: profile me', done => {
+            const Authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAxLCJlbWFpbCI6ImZhdXphbnRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjQ4NDU2ODEyfQ.pcVDNXzE2YXAx4gTv1cpQiAJsrCgeEAZGe2kMDzNDKw';
+            const id = '101';
+            const email = 'fauzantest@gmail.com';
+            request(app)
+                .get('/api/me')
+                .set( 'Authorization' , 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAxLCJlbWFpbCI6ImZhdXphbnRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjQ4NDU2ODEyfQ.pcVDNXzE2YXAx4gTv1cpQiAJsrCgeEAZGe2kMDzNDKw')
+                .then(res => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.headers.Authorization).toMatch( Authorization )
+                    expect(res.body).toHaveProperty('result')
+                    expect(res.body).toHaveProperty('message')
+                    expect(res.body).toHaveProperty('data')
+                    expect(res.body.result).toEqual('Success')
+                    expect(res.body.message).toEqual('Here is your own info')
+                    expect(res.body.data).toHaveProperty('id')
+                    expect(res.body.data).toHaveProperty('email')
+                    expect(res.body.data.id).toEqual(id)
+                    expect(res.body.data.email).toEqual(email)
+                    done();
+                })
+        })
+    })
+
+jest.setTimeout(12000)
+
+    describe('GET /api/all', () => {
+        test('test: all user', done => {
+            const id = '101';
+            const email = 'fauzantest@gmail.com';
+            request(app)
+                .get('/api/all')
+                .then(res => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body).toHaveProperty('result')
+                    expect(res.body).toHaveProperty('message')
+                    expect(res.body).toHaveProperty('data')
+                    expect(res.body.result).toEqual('Success')
+                    expect(res.body.message).toEqual('Showing all available user')
+                    expect(res.body.data).toHaveProperty('id')
+                    expect(res.body.data).toHaveProperty('email')
+                    expect(res.body.data.id).toEqual(id)
+                    expect(res.body.data.email).toEqual(email)
+                    done();
+                })
+        })
+    })
+
+    describe('GET /api/user/101', () => {
+        test('test: user 101', done => {
+            const id = '101';
+            const email = 'fauzantest@gmail.com';
+            const username = 'fauzantest'
+            request(app)
+                .get('/api/user/101')
+                .then(res => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body).toHaveProperty('result')
+                    expect(res.body).toHaveProperty('message')
+                    expect(res.body).toHaveProperty('data')
+                    expect(res.body.result).toEqual('Success')
+                    expect(res.body.message).toEqual('Successfully retrieved user')
+                    expect(res.body.data).toHaveProperty('id')
+                    expect(res.body.data).toHaveProperty('email')
+                    expect(res.body.data).toHaveProperty('username')
+                    expect(res.body.data.id).toEqual(id)
+                    expect(res.body.data.email).toEqual(email)
+                    expect(res.body.data.username).toEqual(username)
                     done();
                 })
         })
