26054e760c9aecc021dda433486f01e833538782
meddhawi <meddhawi@gmail.com>
2022-03-30 19:17:16 +0700

    unit test for register and login has passed!

--- test/authTest.spec.js
+++ test/authTest.spec.js
@@ -2,10 +2,72 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('POST /api/register', () => {
-    test('return status: 400 because of missing credential', done => {})
-    test('return status: 400 because of existing user', done => {})
-    test('return status: 400 because password is less than 6 characters', done => {})
-    test('return status: 400 because email is not in the right format', done => {})
+    test('return status: 400 because of missing credential', done => {
+        const email = 'jk@gmail.com';
+        const password = '123456';
+        request(app)
+            .post('/api/register')
+            .send({ email, password})
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toHaveProperty('result');
+                expect(res.body).toHaveProperty('message');
+                expect(res.body.result).toEqual('failed');
+
+                done();
+            })
+    })
+    test('return status: 400 because of existing user', done => {
+        const email = 'admin@mail.com'
+        const password = 'admin'
+        const username = 'admin'
+        request(app)
+            .post('/api/register')
+            .send({ email, username, password})
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toHaveProperty('result');
+                expect(res.body).toHaveProperty('message');
+                expect(res.body.result).toEqual('failed');
+                expect(res.body.message).toEqual('Email already existed');
+
+                done();
+            })
+    })
+    test('return status: 400 because password is less than 6 characters', done => {
+        const email = 'jk@gmail.com';
+        const password = '12345';
+        const username = 'jk'
+        request(app)
+            .post('/api/register')
+            .send({ email, username, password})
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toHaveProperty('result');
+                expect(res.body).toHaveProperty('message');
+                expect(res.body.result).toEqual('failed');
+                expect(res.body.message).toEqual('Please make your password bigger than 6 characters');
+
+                done();
+            })
+    })
+    test('return status: 400 because email is not in the right format', done => {
+        const email = 'jkmail.com';
+        const password = '123456';
+        const username = 'jk'
+        request(app)
+            .post('/api/register')
+            .send({ email, username, password})
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toHaveProperty('result');
+                expect(res.body).toHaveProperty('message');
+                expect(res.body.result).toEqual('failed');
+                expect(res.body.message).toEqual('Please enter valid email address');
+
+                done();
+            })
+    })
     test('return status: 200 with a register profile', done => {
         const email = 'jk@gmail.com';
         const password = '123456';
@@ -29,7 +91,23 @@ describe('POST /api/register', () => {
 })
 
 describe('POST /api/login', () => {
-    test('return status: 400 login failed because the wrong credentials')
+    test('return status: 400 login failed because the wrong credentials', done => {
+        const email = 'jk@gmail.com';
+        const password = '12346';
+        request(app)
+            .post('/api/login')
+            .send({ email, password})
+            .then(res => {
+                expect(res.statusCode).toBe(400);
+                expect(res.body).toHaveProperty('result');
+                expect(res.body).toHaveProperty('message');
+                expect(res.body.result).toEqual('Login failed!');
+                expect(res.body.message).toEqual('Try entering the right credentials');
+
+                done();
+            })
+            
+    })
     test('return status: 201 login success', done => {
         const email = 'jk@gmail.com';
         const password = '123456';
