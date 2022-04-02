7c434dd778952e1abb1c43c8bc7db80e9f3bae05
meddhawi <meddhawi@gmail.com>
2022-04-02 18:49:47 +0700

    adjusted code so it would match and fixed some format issue with /all

--- test/authTest.spec.js
+++ test/authTest.spec.js
@@ -1,6 +1,8 @@
 const request = require('supertest')
 const app = require('../app')
 
+const Auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJhZG1pbkBtYWlsLmNvbSIsImlhdCI6MTY0ODg5ODAyN30.8ZjtLMMt9Jjf5FGqkoHMEElz--12Tni59ttUoKH8wgs'
+
 describe('POST /api/register', () => {
     test('return status: 400 because of missing credential', done => {
         const email = 'jk@gmail.com';
@@ -18,7 +20,7 @@ describe('POST /api/register', () => {
     })
     test('return status: 400 because of existing user', done => {
         const email = 'admin@mail.com'
-        const password = 'admin'
+        const password = 'admin2'
         const username = 'admin'
         request(app)
             .post('/api/register')
@@ -65,7 +67,7 @@ describe('POST /api/register', () => {
             })
     })
     test('return status: 200 with a register profile', done => {
-        const email = 'jj@gmail.com';
+        const email = '2@gmail.com';
         const password = '123456';
         const username = 'jk'
         request(app)
@@ -131,7 +133,7 @@ describe('Update Score Method', () => {
     test('Score berhasil', done => {
         const gameID = 1;
         const point = 1;
-        const Auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJhZG1pbkBtYWlsLmNvbSIsImlhdCI6MTY0ODcyMTI4NX0.3C3AhVZdrnNx3NAG5xV72gtJtWAvb_uRWTrRiWPWDdI'
+        // const Auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJhZG1pbkBtYWlsLmNvbSIsImlhdCI6MTY0ODcyMTI4NX0.3C3AhVZdrnNx3NAG5xV72gtJtWAvb_uRWTrRiWPWDdI'
         request(app)
             .post('/api/score')
             .set('Authorization', Auth)
@@ -162,4 +164,106 @@ describe('Get Game Details', () => {
         })
     })
 
+    describe('test: token', () => {
+        test('verifytoken', done => {
+            const id = 2;
+            const email = 'admin@mail.com';
+            // const Auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA3LCJlbWFpbCI6ImZhdXphbnRlc3QxQGdtYWlsLmNvbSIsImlhdCI6MTY0ODgwMDAxMn0.YARVD5GM4lDaUWf4Xg0wGaG2JqcCryOIib4SlD5-_3w'
+            request(app)
+                .get('/api/verifyToken')
+                .set('Authorization', Auth)
+                // .send({gameID, point})
+                .then(res => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.result).toHaveProperty('err')
+                    expect(res.body.result).toHaveProperty('result')
+                    expect(res.body.result.result).toHaveProperty('id')
+                    expect(res.body.result.result).toHaveProperty('email')
+                    expect(res.body.result.result.id).toEqual(id)
+                    expect(res.body.result.result.email).toEqual(email)
+                    done();
+                })
+            })
+        })
+    
+        describe('GET /api/me', () => {
+            test('test: profile me', done => {
+                // const Auth = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAxLCJlbWFpbCI6ImZhdXphbnRlc3RAZ21haWwuY29tIiwiaWF0IjoxNjQ4NDU2ODEyfQ.pcVDNXzE2YXAx4gTv1cpQiAJsrCgeEAZGe2kMDzNDKw';
+                const id = 2;
+                const username = 'admin';
+                const email = 'admin@mail.com';
+                request(app)
+                    .get('/api/me')
+                    .set( 'Authorization' , Auth)
+                    .then(res => {
+                        expect(res.statusCode).toBe(200)
+                        expect(res.body).toHaveProperty('result')
+                        expect(res.body).toHaveProperty('message')
+                        expect(res.body).toHaveProperty('data')
+                        expect(res.body.result).toEqual('success')
+                        expect(res.body.message).toEqual('Here is your own info')
+                        expect(res.body.data).toHaveProperty('id')
+                        expect(res.body.data).toHaveProperty('username')
+                        expect(res.body.data).toHaveProperty('email')
+                        expect(res.body.data.id).toEqual(id)
+                        expect(res.body.data.username).toEqual(username)
+                        expect(res.body.data.email).toEqual(email)
+                        done();
+                    })
+            })
+        })
+    
+    describe('GET /api/me', () => {
+        test('return status: 401 Unauthorized', (done) => {
+            request(app)
+            .get('/api/me')
+            .then((res) => {
+                expect(res.statusCode).toBe(401)
+                done()
+            })
+        })
+    })
+
+    // jest.setTimeout(12000)
+    
+    describe('GET /api/all', () => {
+        test('test: all user', done => {
+            request(app)
+                .get('/api/all')
+                .then(res => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body).toHaveProperty('result')
+                    expect(res.body).toHaveProperty('message')
+                    expect(res.body).toHaveProperty('data')
+                    expect(res.body.result).toEqual('success')
+                    expect(res.body.message).toEqual('Showing all available user')
+                    done();
+                })
+        })
+    })
+
+    describe('GET /api/user/101', () => {
+        test('test: user 101', done => {
+            const id = 2;
+            const email = 'admin@mail.com';
+            const username = 'admin'
+            request(app)
+                .get('/api/user/2')
+                .then(res => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body).toHaveProperty('result')
+                    expect(res.body).toHaveProperty('message')
+                    expect(res.body).toHaveProperty('data')
+                    expect(res.body.result).toEqual('success')
+                    expect(res.body.message).toEqual('Successfully retrieved user')
+                    expect(res.body.data).toHaveProperty('id')
+                    expect(res.body.data).toHaveProperty('email')
+                    expect(res.body.data).toHaveProperty('username')
+                    expect(res.body.data.id).toEqual(id)
+                    expect(res.body.data.email).toEqual(email)
+                    expect(res.body.data.username).toEqual(username)
+                    done();
+                })
+        })
+    })
 
